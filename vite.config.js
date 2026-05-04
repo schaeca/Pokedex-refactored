1aa9349508b37f279af2463ea2142e42a148ddba
Carina Schädlich <carina.schaedlich@gmail.com>
2026-05-04 12:04:21 +0200

    configured vite.config.js for multipages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [tailwindcss()]
+  plugins: [tailwindcss()],
+   build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'pokedex.html'),
+        // nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
 });
